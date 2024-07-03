5e2b436703d10102620c02490124690114dbe8cf
semar <semaraugusto@gmail.com>
2024-07-03 11:07:35 -0300

    add some l2 contracts back into l2genesis deploy

--- packages/contracts-bedrock/src/libraries/Predeploys.sol
+++ packages/contracts-bedrock/src/libraries/Predeploys.sol
@@ -152,29 +152,28 @@ library Predeploys {
 
   /// @notice Returns true if the predeploy is not used in fraud proofs.
   function notFraudProof(address _addr) internal pure returns (bool) {
-    return true;
-    // return
-    //   _addr == LEGACY_MESSAGE_PASSER ||
-    //   _addr == L1_MESSAGE_SENDER ||
-    //   _addr == DEPLOYER_WHITELIST ||
-    //   _addr == L2_CROSS_DOMAIN_MESSENGER ||
-    //   _addr == GAS_PRICE_ORACLE ||
-    //   _addr == L2_STANDARD_BRIDGE ||
-    //   _addr == SEQUENCER_FEE_WALLET ||
-    //   _addr == OPTIMISM_MINTABLE_ERC20_FACTORY ||
-    //   _addr == L2_ERC721_BRIDGE ||
-    //   _addr == L1_BLOCK_ATTRIBUTES ||
-    //   _addr == L2_TO_L1_MESSAGE_PASSER ||
-    //   _addr == OPTIMISM_MINTABLE_ERC721_FACTORY ||
-    //   _addr == PROXY_ADMIN ||
-    //   _addr == BASE_FEE_VAULT ||
-    //   _addr == L1_FEE_VAULT ||
-    //   _addr == SCHEMA_REGISTRY ||
-    //   _addr == EAS ||
-    //   _addr == GOVERNANCE_TOKEN ||
-    //   _addr == LEGACY_ERC20_ETH ||
-    //   _addr == CROSS_L2_INBOX ||
-    //   _addr == L2_TO_L2_CROSS_DOMAIN_MESSENGER;
+    return
+      // _addr == DEPLOYER_WHITELIST ||
+      // _addr == GAS_PRICE_ORACLE ||
+      // _addr == L1_BLOCK_ATTRIBUTES ||
+      _addr == LEGACY_MESSAGE_PASSER ||
+      _addr == L1_MESSAGE_SENDER ||
+      _addr == L2_CROSS_DOMAIN_MESSENGER ||
+      _addr == L2_STANDARD_BRIDGE ||
+      _addr == SEQUENCER_FEE_WALLET ||
+      _addr == OPTIMISM_MINTABLE_ERC20_FACTORY ||
+      _addr == L2_ERC721_BRIDGE ||
+      _addr == L2_TO_L1_MESSAGE_PASSER ||
+      _addr == OPTIMISM_MINTABLE_ERC721_FACTORY ||
+      _addr == PROXY_ADMIN ||
+      _addr == BASE_FEE_VAULT ||
+      _addr == L1_FEE_VAULT ||
+      _addr == SCHEMA_REGISTRY ||
+      _addr == EAS ||
+      _addr == GOVERNANCE_TOKEN ||
+      _addr == LEGACY_ERC20_ETH ||
+      _addr == CROSS_L2_INBOX ||
+      _addr == L2_TO_L2_CROSS_DOMAIN_MESSENGER;
   }
 
   /// @notice Returns true if the address is a defined predeploy that is embedded into new OP-Stack chains.
